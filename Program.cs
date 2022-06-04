48177c04074acf0a6b8dd228554b8e2a722d122a
Troy <wilson.troy.anthony@gmail.com>
2022-06-04 07:04:10 -0400

    Added  comments

--- Program.cs
+++ Program.cs
@@ -5,17 +5,17 @@ namespace Deliverable2 {
     internal class Program {
         static void Main(string[] args)
         {
-            string drink1 = "wine";
-            int numOfDrink1 = 0;
-            const double priceForDrink1 = 2.00;
-            string drink2 = "water";
-            int numOfDrink2 = 0;
-            const int maxPeople = 6;
-            int numOfPeople = 0;
-            const double pricePerPerson = 9.99;
+            string drink1 = "wine"; // Sets the expensive drink
+            int numOfDrink1 = 0; // Tracks the number of drinks that are orded
+            const double priceForDrink1 = 2.99; // Cost of the drink
+            string drink2 = "water"; // Sets the free drink
+            int numOfDrink2 = 0; // Tracks how many free drinks are orded
+            const int maxPeople = 6; // Sets the max number of people
+            int numOfPeople = 0; // Tracks how many people we're seating
+            const double pricePerPerson = 9.99; // Sets the cost of the buffet
 
             Console.WriteLine($"Hello, Welcome to the best buffet ever. All you can eat for ${pricePerPerson:0.00}!\nWe do charge {priceForDrink1:0.00} extra for {drink1} but {drink2} is on the house.");
-            Console.WriteLine($"How many will be joining us tonight? The most we can seat is {maxPeople}");
+            Console.WriteLine($"How many will be joining us tonight? The most we can seat is {maxPeople}.");
 
             numOfPeople = int.Parse(Console.ReadLine());
             if(!Enumerable.Range(1, 6).Contains(numOfPeople))
@@ -27,11 +27,11 @@ namespace Deliverable2 {
             Console.WriteLine($"\nA table for {numOfPeople}! Please follow me and take a seat.");
             Console.WriteLine($"Let's get everyone started with some drinks. We've got {drink1} or {drink2}.\n");
 
-            for(int i = 1; i <= numOfPeople; i++)
+            for(int i = 1; i <= numOfPeople; i++) // Asks for each party members drinks
             {
                 Console.WriteLine($"Alright person number {i}, {drink1} or {drink2}?");
 
-                string drink = Console.ReadLine();
+                string drink = Console.ReadLine().ToLower();
 
                 if(drink == drink1)
                 {
@@ -48,20 +48,25 @@ namespace Deliverable2 {
                     Console.WriteLine("Sorry we don't have that drink.\n");
                 }
             }
-            double totalPrice = numOfDrink1 * priceForDrink1 + numOfPeople * pricePerPerson;
 
+            double totalPrice = numOfDrink1 * priceForDrink1 + numOfPeople * pricePerPerson;  // Totals the cost
 
-            if(numOfDrink1 > 0 && numOfDrink2 > 0)
+            if(numOfDrink1 == 6) // Because you have to have an easter egg
+            {
+                Console.WriteLine("Looks like you guys are having a party tonight!");
+            }
+
+            if(numOfDrink1 > 0 && numOfDrink2 > 0) // Lists what drinks where ordered
             {
                 Console.Write($"Okay, so that's {numOfDrink1} {drink1}");
                 if(numOfDrink1 > 1)
                 {
-                    Console.Write("'s");
+                    plural();
                 }
                 Console.Write($" and {numOfDrink2} {drink2}");
                 if(numOfDrink2 > 1)
                 {
-                    Console.Write("'s");
+                    plural();
                 }
             }
             else if(numOfDrink1 > 0)
@@ -69,21 +74,18 @@ namespace Deliverable2 {
                 Console.Write($"Okay, so that's {numOfDrink1} {drink1}");
                 if(numOfDrink1 > 1)
                 {
-                    Console.Write("'s");
+                    plural();
                 }
-            }else if(numOfDrink2 > 0)
+            }else
             {
                 Console.Write($"Okay, so that's {numOfDrink2} {drink2}");
                 if(numOfDrink2 > 1)
                 {
-                    Console.Write("'s");
+                    plural();
                 }
             }
             Console.WriteLine($". I'll be right back. Feel free to grab your food!");
-
-
-
-            Console.WriteLine($"Here's your bill! Total price is: {totalPrice}\n");
+            Console.WriteLine($"Here's your bill! Total price is: {totalPrice}\n"); // Breaks down the bill
             Console.WriteLine($"{numOfPeople} buffets = {numOfPeople} X {pricePerPerson} = {numOfPeople * pricePerPerson}");
             if(numOfDrink1 > 0)
             {
@@ -94,6 +96,11 @@ namespace Deliverable2 {
                 Console.WriteLine($"{numOfDrink2} {drink2} = Free");
             }
             Console.WriteLine($"Total = {totalPrice}");
+        }
+
+        static void plural() // Add 's if more then one drink is ordered
+        {
+            Console.Write("'s");
         }
     }
 }
